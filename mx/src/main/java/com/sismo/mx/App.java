1b0863f0981957df320f0766d69cf25c102efbb9
hpolanco29 <hpolanco@iwa.com.mx>
2017-09-24 17:47:37 -0500

    jar to war transformation

--- mx/src/main/java/com/sismo/mx/App.java
+++ mx/src/main/java/com/sismo/mx/App.java
@@ -3,21 +3,31 @@ package com.sismo.mx;
 import javax.persistence.EntityManagerFactory;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 
+@EnableAutoConfiguration
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
-	}
-	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+    
 	@Bean
 	public HibernateJpaSessionFactoryBean sessionFactory(EntityManagerFactory emf) {
 	    HibernateJpaSessionFactoryBean factory = new HibernateJpaSessionFactoryBean();
 	    factory.setEntityManagerFactory(emf);
 	    return factory;
 	}
+
 }
